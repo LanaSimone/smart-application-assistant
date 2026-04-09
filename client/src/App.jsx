d68ceb2c9f2209c5aa8154be6f2ac579614d9032
Allana DeCarish <allanadecarishs@gmail.com>
2026-04-08 22:48:34 -0400

    Implement application form with submit and backend integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,22 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const initialFormData = {
+  company: "",
+  role: "",
+  status: "",
+  location: "",
+  salary: "",
+  jobLink: "",
+  dateApplied: "",
+  description: "",
+  notes: ""
+}
+
 function App() {
-  const [applications, setApplications] = useState([
-    {id: 1, company: "test co", role: "qa engineer", status: "Applied"}
-  ])
+  const [applications, setApplications] = useState([])
 
-  const [formData, setFormData] = useState({
-    company: "",
-    role: "",
-    status: "Applied",
-    location: "",
-    salary: "",
-    jobLink: "",
-    dateApplied: "",
-    description: "",
-    notes: "",
-
-  })
+  const [formData, setFormData] = useState(initialFormData)
 
   async function fetchApplications() {
     const response = await fetch("http://localhost:5000/api/applications");
@@ -25,6 +24,7 @@ function App() {
     setApplications(data)
   }
 
+  
   function handleInputChange(event){
     const {name, value} = event.target;
     setFormData ({
@@ -32,26 +32,82 @@ function App() {
       [name]: value
     });
   }
-
+  
   useEffect(() => {
     fetchApplications();
   }, [])
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:5000/api/applications", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+      
+    })
+
+    const newApplication = await response.json()
+
+    setApplications((currentApplications) =>[
+      newApplication,
+      ...currentApplications
+    ])
+    setFormData(initialFormData);
+  }
+  
   return (
     <div>
       <h1>Smart Application Assistant</h1>
       <h2>Applications</h2>
-      <input
-      name="company"
-      placeholder="Company"
-      onChange={handleInputChange}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+        name="company"
+        placeholder="Company"
+        value={formData.company}
+        onChange={handleInputChange}
+        />
+        <input
+        name="role"
+        placeholder="Position"
+        value={formData.role}
+        onChange={handleInputChange}
+        />
+        <input
+        name="location"
+        placeholder="Location"
+        value={formData.location}
+        onChange={handleInputChange}
+        />
+        <select
+        name="status"
+        value={formData.status}
+        onChange={handleInputChange}
+        >
+          <label>
+            Select Current Status
+            <option value="Applied">Applied</option>
+            <option value="Interviewing">Interviewing</option>
+            <option value="Offer">Offer</option>
+            <option value="Rejected">Rejected</option>
+          </label>
+        </select>
+            
+        <button type="submit">Save</button>
+      </form>
+
       <p>Company: {formData.company}</p>
+      <p>Role: {formData.role}</p>
+      <p>Location: {formData.location}</p>
+      <p>Status: {formData.status}</p>
       
       {applications.map((app) => (
         <div key ={app.id}>
-          <p>{app.company}</p>
-          <p>{app.role}</p>
+          <p>Company: {app.company}</p>
+          <p>Role: {app.role}</p>
+          <p>Location:{app.location}</p>
           <p>Status: {app.status}</p>
         </div>
       ))}
